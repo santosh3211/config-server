f001384d18465c270b8ebe6d9227fe9e42ea9a87
santosh3211 <santoshprakashsahu3@gmail.com>
2021-06-15 22:11:49 +0530

    Config server set up

---
+++ src/test/java/com/epam/cloud/config/ClouldConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.epam.cloud.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClouldConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
